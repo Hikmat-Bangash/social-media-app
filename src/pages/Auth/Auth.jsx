740abd42904a1895ff5129f21d8fadca44c084da
Hikmat Bangash <100120213+Hikmat-Bangash@users.noreply.github.com>
2023-05-25 08:46:37 -0700

    Update Auth.jsx

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -83,7 +83,7 @@ const Auth = () => {
       <div className="a-left">
         <img src={Logo} alt="" />
         <div className="Webname">
-          <h1>HKB Media</h1>
+          <h1>HB Media</h1>
           <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
